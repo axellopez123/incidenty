029ee74f73fc623f391d78bf1903e80b3888de92
Axel Lopez <ballardo.lopez123@gmail.com>
2026-03-01 23:15:03 -0600

    *

--- app/distances/models/distance.py
+++ app/distances/models/distance.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 from app.database import Base
-from app.events.models.event_categories import EventCategory
 
 class Distance(Base):
     __tablename__ = "distances"
